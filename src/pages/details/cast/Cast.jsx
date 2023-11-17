23efd315a1c862cd0abaea64e96d0162a08486df
Melody1106 <101308244+Melody1106@users.noreply.github.com>
2023-11-17 13:34:48 +0800

    卡司串接

--- src/pages/details/cast/Cast.jsx
+++ src/pages/details/cast/Cast.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-function Cast() {
+import './style.scss';
+
+import ContentWrapper from '../../../components/contentWrapper/ContentWrapper';
+import Image from '../../../components/lazyLoadImage/Image';
+import avatar from '../../../assets/avatar.png';
+
+function Cast({ data, loading }) {
+  const { url } = useSelector((state) => state.movie);
+
   return (
     <div className="castSection">
-      <div>Top Cast</div>
+      <div className="sectionHeading">Top Cast</div>
+      <ContentWrapper>
+        {!loading ? (
+          <div className="listItems">
+            {data?.map((v) => {
+              let imgUrl = v.profile_path
+                ? url.profile + v.profile_path
+                : avatar;
+              return (
+                <div key={v.id} className="listItem">
+                  <div className="profileImg">
+                    <Image srcPop={imgUrl} />
+                  </div>
+                  <div className="name">{v.name}</div>
+                  <div className="character">{v.character}</div>
+                </div>
+              );
+            })}
+          </div>
+        ) : (
+          <div>loading...</div>
+        )}
+      </ContentWrapper>
     </div>
   );
 }
