b1e0428513e2bf78eecc094529cc3a6a84f315e8
Melody1106 <101308244+Melody1106@users.noreply.github.com>
2023-11-22 01:14:33 +0800

    logo c

---
+++ src/components/videoPop/VideoPop.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function VideoPop() {
+  return (
+    <div>
+      <div className="opacityLayer">
+        <div className="videoPlayer">
+          <span>close</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default VideoPop;
